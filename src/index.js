e4c9f706ead9c7b71a3db25b6046fa044197827d
Lucas Marcos <lucasmarcos@live.com>
2020-09-25 15:08:56 -0300

    uma estrutura de navegacao simples e uma idea de layout com flexbox

--- src/index.js
+++ src/index.js
@@ -1,11 +1,54 @@
 import ReactDOM from "react-dom";
 
+const root = document.getElementById("root");
+const changeRoot = (component) => ReactDOM.render(component(), root);
+
+const SideBar = () => {
+  return (
+    <div className="sidebar">
+    </div>
+  )
+}
+
+const Administrador = () => {
+  return (
+    <>
+      <SideBar/>
+      <div className="main"></div>
+    </>
+  )
+}
+
+const Tecnico = () => {
+  return (
+    <div className="main"></div>
+  )
+}
+
+const Proprietario = () => {
+  return (
+    <div className="main"></div>
+  )
+}
+
 const App = () => {
   return (
     <div>
+      <ul>
+        <li onClick={() => changeRoot(Administrador)}>
+          Administrador
+        </li>
+
+        <li onClick={() => changeRoot(Tecnico)}>
+          Tecnico
+        </li>
+
+        <li onClick={() => changeRoot(Proprietario)}>
+          Proprietario
+        </li>
+      </ul>
     </div>
   );
 };
 
-const root = document.getElementById("root");
-ReactDOM.render(<App/>, root);
+changeRoot(App);
